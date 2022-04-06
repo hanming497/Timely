581b2483e753736990df010f3bb8a39d75e68ee4
Han Liu <hanming497@gmail.com>
2022-04-05 19:39:52 -0600

    templates/home

--- Timely/main_app/views.py
+++ Timely/main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+    return render(request, 'home.html')
