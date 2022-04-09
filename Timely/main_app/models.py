40ca3f4c185f1e0b4e7e1081ae63553f9e27fde5
Han Liu <hanming497@gmail.com>
2022-04-09 16:10:27 -0600

    working migrations - timezones

--- Timely/main_app/models.py
+++ Timely/main_app/models.py
@@ -1,18 +1,13 @@
-import pendulum
 from django.db import models
 from django.urls import reverse
 from datetime import date
-# Import the User
-from django.contrib.auth.models import User
 
 
 class Timezones(models.Model):
-    timezone: models.CharField(max_length=150)
-    user: models.ForeignKey(User, on_delete=models.CASCADE)
-    hourToggle: models.BooleanField(default=False)
-    availabilityStartTime: models.CharField(max_length=150, default='00:00')
-    availabilityEndTime: models.CharField(max_length=150, default='23:59')
+    timezone= models.CharField(max_length=100)
+    hour_toggle= models.BooleanField(default=False)
+    availability_start_time= models.CharField(max_length=100, default='00:00')
+    availability_end_time= models.CharField(max_length=100, default='23:59')
 
-    def __str__(self):
-        return self.timezone + ' - ' + self.availabilityStartTime + ' - ' + self.availabilityEndTime
-    
+    # def __str__(self):
+    #     return self.timezone + ' - ' + self.availabilityStartTime + ' - ' + self.availabilityEndTime
