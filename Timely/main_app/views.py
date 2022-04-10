e6570d111fd3838ae82576faedf60019f4d98b51
akndesign <alexanderkoziol@hotmail.com>
2022-04-09 21:04:54 -0700

    complete design of sign-up page
    will need django links added back in, and possible autocomplete field for cities to input. for now it's a dropdown

--- Timely/main_app/views.py
+++ Timely/main_app/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Define the home view
+
+
 def home(request):
     return render(request, 'home.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
 
 def timezones(request):
     return render(request, 'timezones.html', {'timezones': timezones})
