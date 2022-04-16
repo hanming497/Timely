dd1bc4e271b8b57507b9b54abf85f09f0f3eb430
akndesign <alexanderkoziol@hotmail.com>
2022-04-16 00:26:27 -0700

    Change to Signup and Defaults for New Timezone

--- Timely/main_app/models.py
+++ Timely/main_app/models.py
@@ -6,15 +6,17 @@ from django.contrib.auth.models import User
 
 
 class Timezones(models.Model):
-    timezone = models.ForeignKey('iana_timezones', on_delete=models.CASCADE, null=True)
+    timezone = models.ForeignKey(
+        'iana_timezones', on_delete=models.CASCADE, null=True)
     hour_toggle = models.BooleanField(default=False)
     availability_start_time = models.CharField(max_length=100, default='00:00')
     availability_end_time = models.CharField(max_length=100, default='23:59')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    city = models.CharField(max_length=100)
-    country = models.ForeignKey('Country', on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=100)
-    
+    city = models.CharField(max_length=100, default='Toronto')
+    country = models.ForeignKey(
+        'Country', on_delete=models.CASCADE, null=True, default='Canada')
+    name = models.CharField(max_length=100, default='Princeton')
+
     def __str__(self):
         return self.name + ' - ' + str(self.timezone) + ' - ' + str(self.city) + ' - ' + self.availability_start_time + ' - ' + self.availability_end_time + ' - ' + str(self.hour_toggle)
 
